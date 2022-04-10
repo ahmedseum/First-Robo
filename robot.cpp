a938bfc443eefb7b8809a7d72086875f3c95d8ea
ahmedseum <100505704+ahmedseum@users.noreply.github.com>
2022-04-11 00:01:33 +0600

    Add files via upload

---
+++ robot.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int j=0;j<100;j++)
+{
+
+system("CLS");
+cout<<"                   |___|   "<<endl;
+cout<<"                   (- -)"<<endl;
+cout<<"                    \ /"<<endl;
+cout<<"          | |  d----------b"<<endl;
+cout<<"           \   / |       | \ "<<endl;
+cout<<"            \ /  |       |  \ "<<endl;
+cout<<"             V   |       |   |"<<endl;
+cout<<"                 |       |   |"<<endl;
+cout<<"                 ---------  | |"<<endl;
+cout<<"                 |  | |  |"<<endl;
+cout<<"                 |  | |  |"<<endl;
+cout<<"                 |  | |  |"<<endl;
+
+for(int i=0;i<3;i++)
+{
+cout<<"                    --- "<<endl;
+cout<<"                   (   )"<<endl;
+cout<<"                    | |"<<endl;
+system("CLS");
+}
+}
+
+
+
+    return 0;
+}
